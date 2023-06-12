970b9178d86298629732eb35b8d1d6dce8cc0472
Srihari <sshsrn@gmail.com>
2023-06-13 01:31:34 +0530

    feat: added unity model
    Signed-off-by: Srihari <sshsrn@gmail.com>

--- frontend/src/components/Mainpage.js
+++ frontend/src/components/Mainpage.js
@@ -1,9 +1,22 @@
 import Navbar from "./Navbar"
+import "./Mainpage.css"
+import { useEffect } from "react"
+import $ from 'jquery';
+
 
 const Mainpage = () => {
+    useEffect(() => {
+        $(document).ready(function () {
+            console.log("ready!");
+            $('iframe').on('load', function () {
+                console.log($('iframe').contents().find('html').html())
+            });
+        });
+    }, [])
     return (
         <div>
             <Navbar />
+            <iframe scrolling="no" className='unityFrame' src="https://v6p9d9t4.ssl.hwcdn.net/html/8096872/index.html" title="myFrame"></iframe>
         </div>
     )
 }
