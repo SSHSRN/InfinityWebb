e87a37529a8f2e7c1718f5b81908bc940a349fea
ec7719 <ec7719@srmist.edu.in>
2023-06-25 19:56:28 +0530

    added Mongo functionality

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import pymongo
 
 app = Flask(__name__)
 CORS(app)
@@ -10,11 +11,15 @@ def hello():
 
 @app.route('/signup', methods=['POST'])
 def signup():
-    data=request.json
-    resStr = "Hello "+data['name']+" your email is "+data['email']
-    return jsonify({'message': resStr})
+    client = pymongo.MongoClient("mongodb://localhost:27017")
+    database = client["db"]
+    cluster = database["collection"]
+    data = request.json
+    Query=cluster.insert_one(data)
+    if Query:
+        return jsonify({"message": "User created successfully"})
 
-# Run Server in port 5000
+# Run Server on port 5000
 if __name__ == '__main__':
     app.run(host='localhost', port=5000, debug=True)
-    print('Server running in port 5000')
+    print('Server running on port 5000')
