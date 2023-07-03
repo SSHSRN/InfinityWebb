a55a81e8ed0fffeb58f8243a7211af5cf06785ca
Srihari <sshsrn@gmail.com>
2023-07-03 22:15:57 +0530

    feat: added result15
    Signed-off-by: Srihari <sshsrn@gmail.com>

---
+++ frontend/src/components/textgame/Result15.js
@@ -0,0 +1,26 @@
+// This will be shown if the player did not take the ring from the noble looking corpse in the earlier situation.
+
+import React, { useEffect } from 'react';
+import './textgameStyles.css';
+
+const Result15 = () => {
+    useEffect(() => {
+        document.querySelector('.survivalGame').style.backgroundImage = 'url("./assets/result15.jpg")';
+        // const audio = document.querySelector('#result15Audio');
+        // if (audio.paused) {
+        //     audio.play();
+        // }
+    }, []);
+
+    return (
+        <div className='result result15'>
+            <p className='resultText result15Text'>
+                You are captured by the aliens, who are amazed by your agility and fighting abilities. In order to harness your skills, the aliens erase your memories and groom you to become a soldier in their formidable army. From this point on, you pledge your allegiance to the alien cause and begin a journey of ascending through the ranks of their military.
+            </p>
+            <button className='homeButton' onClick={() => window.location.reload()}> Play Again </button>
+            {/* <audio id="result15Audio" src="https://cdn10.solamutha.xyz/download/MsONNlECYNV9fbULaNmuJQ/1688224751/t/2016/Kabali/128/Neruppu-Da.mp3" /> */}
+        </div>
+    )
+}
+
+export default Result15
