0c0971c152f45d30eefb872c4892c23cf059df48
Srihari <sshsrn@gmail.com>
2023-07-05 02:39:21 +0530

    feat: added result30
    Signed-off-by: Srihari <sshsrn@gmail.com>

---
+++ frontend/src/components/textgame/Result30.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import './textgameStyles.css';
+import Result29 from './Result29';
+
+const Result30 = () => {
+    useEffect(() => {
+        document.querySelector('.survivalGame').style.backgroundImage = 'url("./assets/result30.jpg")';
+        // const audio = document.querySelector('#result30Audio');
+        // if (audio.paused) {
+        //     audio.play();
+        // }
+    }, []);
+
+    const [showNext, setShowNext] = useState(false);
+
+    const handleNextClick = () => {
+        setShowNext(true);
+    };
+
+    if (showNext) {
+        return <Result29 />;
+    }
+
+    return (
+        <div className='result result30'>
+            <p className='resultText result30Text'>
+                As you had taken a few gems from the treasure chest earlier, you use them to buy the spaceship parts you need. You buy the parts and repair your spaceship.
+            </p>
+            <button className="nextButton" onClick={handleNextClick}>
+                Next &#8594;
+            </button>
+            {/* <audio id="result30Audio" src="https://cdn10.solamutha.xyz/download/MsONNlECYNV9fbULaNmuJQ/1688224751/t/2016/Kabali/128/Neruppu-Da.mp3" /> */}
+        </div>
+    )
+}
+
+export default Result30
