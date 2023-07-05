6a1c15c8c5f8c1751bc41cf7d8aa9943efdf54e5
Srihari <sshsrn@gmail.com>
2023-07-05 19:34:16 +0530

    feat: added result 33
    Signed-off-by: Srihari <sshsrn@gmail.com>

---
+++ frontend/src/components/textgame/Result33.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import './textgameStyles.css';
+import Result29 from './Result29';
+
+const Result33 = () => {
+    useEffect(() => {
+        document.querySelector('.survivalGame').style.backgroundImage = 'url("./assets/result33.jpg")';
+        // const audio = document.querySelector('#result33Audio');
+        // if (audio.paused) {
+        //     audio.play();
+        // }
+    }, []);
+
+    const [showNext, setShowNext] = useState(false);
+
+    const handleNextClick = () => {
+        setShowNext(true);
+    };
+
+    if (showNext) {
+        return <Result29 />;
+    }
+
+    return (
+        <div className='result result33'>
+            <p className='resultText result33Text'>
+                You are very happy that you are finally home,but you realize that your family may not be able to recognize this new appearance of yours. Hence, you go to your space research center and try to meet your senior scientist, but fearing your appearance, he tranquilizes you even before you can speak something.
+            </p>
+            <button className="nextButton" onClick={handleNextClick}>
+                Next &#8594;
+            </button>
+            {/* <audio id="result33Audio" src="https://cdn10.solamutha.xyz/download/MsONNlECYNV9fbULaNmuJQ/1688224751/t/2016/Kabali/128/Neruppu-Da.mp3" /> */}
+        </div>
+    )
+}
+
+export default Result33
