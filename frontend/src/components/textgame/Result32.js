86aa8c4221d6e74bcb92dba022809bf330bfecc3
Srihari <sshsrn@gmail.com>
2023-07-05 19:23:30 +0530

    feat: added result 32
    Signed-off-by: Srihari <sshsrn@gmail.com>

---
+++ frontend/src/components/textgame/Result32.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import './textgameStyles.css';
+import Result29 from './Result29';
+
+const Result32 = () => {
+    useEffect(() => {
+        document.querySelector('.survivalGame').style.backgroundImage = 'url("./assets/result32.jpg")';
+        // const audio = document.querySelector('#result32Audio');
+        // if (audio.paused) {
+        //     audio.play();
+        // }
+    }, []);
+
+    const [showNext, setShowNext] = useState(false);
+
+    const handleNextClick = () => {
+        setShowNext(true);
+    };
+
+    if (showNext) {
+        return <Result29 />;
+    }
+
+    return (
+        <div className='result result32'>
+            <p className='resultText result32Text'>
+                You are very happy that you are finally home,but joy soon turns to despair as your family do not recognize you and scream in fear at your hideous appearance. Although you try to explain, your altered voice only causes more panic. The military arrive at the house and shoot you with tranquilizers.
+            </p>
+            <button className="nextButton" onClick={handleNextClick}>
+                Next &#8594;
+            </button>
+            {/* <audio id="result32Audio" src="https://cdn10.solamutha.xyz/download/MsONNlECYNV9fbULaNmuJQ/1688224751/t/2016/Kabali/128/Neruppu-Da.mp3" /> */}
+        </div>
+    )
+}
+
+export default Result32
