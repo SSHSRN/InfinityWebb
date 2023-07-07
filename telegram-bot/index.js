b7c8a841fc531b58301c926e80ffb2f2b18b8ca3
Srihari <sshsrn@gmail.com>
2023-07-08 01:50:59 +0530

    feat: added apod function
    Signed-off-by: Srihari <sshsrn@gmail.com>

--- telegram-bot/index.js
+++ telegram-bot/index.js
@@ -3,7 +3,26 @@ const { Telegraf } = require('telegraf');
 require('dotenv').config();
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_API_KEY);
-// Create a .env file in the project root folder and save the telegram-bot api key there.
+const apodUrl = process.env.APOD_URL;
+
+bot.hears('hi', (ctx) => ctx.reply('Hey there!'));
+
+bot.command('start', (ctx) => {
+    ctx.reply('Welcome to Space Bot 🤖');
+    ctx.reply('Type /apod to get the Astronomy Picture of the Day');
+});
+
+bot.command('apod', async (ctx) => {
+    try {
+        const response = await axios.get(apodUrl);
+        const { title, url, explanation } = response.data;
+        ctx.replyWithPhoto(url, { caption: `${title}\n\n${explanation}` });
+    } catch (error) {
+        console.log(error);
+        ctx.reply('Sorry, something went wrong!');
+    }
+});
+
 
 // Launch the bot
 bot.launch();
