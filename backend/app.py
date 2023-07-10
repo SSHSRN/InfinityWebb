05c55bc4bcc40241b687350922ded4a7df32cc4d
Srihari <sshsrn@gmail.com>
2023-07-10 21:22:33 +0530

    fix: fixed backend port error
    Signed-off-by: Srihari <sshsrn@gmail.com>

--- backend/app.py
+++ backend/app.py
@@ -118,5 +118,5 @@ def sunriseSet():
 
 # Run Server on port 5000
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
     print('Server running on port 5000')
