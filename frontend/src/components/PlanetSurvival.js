ba0bfdef7266b67d1456c91f6e43d433308f85a3
Srihari <sshsrn@gmail.com>
2023-07-10 16:00:08 +0530

    feat: added Go To Home Button from game page
    Signed-off-by: Srihari <sshsrn@gmail.com>

--- frontend/src/components/PlanetSurvival.js
+++ frontend/src/components/PlanetSurvival.js
@@ -6,8 +6,6 @@ import axios from 'axios'
 
 const PlanetSurvival = () => {
   const [showGame, setShowGame] = useState(false);
-  const [showInstructions, setShowInstructions] = useState(false);
-  const [showSettings, setShowSettings] = useState(false);
 
   useEffect(() => {
     if (navigator.onLine) {
@@ -35,14 +33,6 @@ const PlanetSurvival = () => {
     setShowGame(true);
   };
 
-  const handleInstructionsClick = () => {
-    setShowInstructions(true);
-  };
-
-  const handleSettingsClick = () => {
-    setShowSettings(true);
-  };
-
   const renderSituation = () => {
     return (
       <div className="survivalGame__play">
@@ -59,55 +49,17 @@ const PlanetSurvival = () => {
           renderSituation()
         ) : (
           <div className="survivalGame__menu">
-            <button className="survivalGame__menu__item__button" onClick={handleInstructionsClick}>
-              Instructions
-            </button>
             <button className="survivalGame__menu__item__button" onClick={handlePlayClick}>
               Play
-            </button>
-            <button className="survivalGame__menu__item__button" onClick={handleSettingsClick}>
-              Settings
             </button>
           </div>
         )}
       </div>
-      {showInstructions && (
-        <div className="modal is-active">
-          <div className="modal-background"></div>
-          <div className="modal-card">
-            <header className="modal-card-head">
-              <p className="modal-card-title">Instructions</p>
-              <button
-                className="delete"
-                aria-label="close"
-                onClick={() => setShowInstructions(false)}
-              ></button>
-            </header>
-            <section className="modal-card-body">
-              <p>Instructions da</p>
-            </section>
-          </div>
-        </div>
-      )}
-
-      {showSettings && (
-        <div className="modal is-active">
-          <div className="modal-background"></div>
-          <div className="modal-card">
-            <header className="modal-card-head">
-              <p className="modal-card-title">Settings</p>
-              <button
-                className="delete"
-                aria-label="close"
-                onClick={() => setShowSettings(false)}
-              ></button>
-            </header>
-            <section className="modal-card-body">
-              <p>Settings da...</p>
-            </section>
-          </div>
-        </div>
-      )}
+      <div className="goToHome">
+        <button className="goToHome__button" onClick={() => window.location.replace("/")}>
+          Go to Home
+        </button>
+      </div>
       <div className="gradient-overlay"></div>
       <video src={'assets/bifrostVideo.mp4'} className="bifrostVideo is-hidden" muted></video>
     </div>
